b249f2c54c3739d82eaccd8204e8b1f0d72229d5
helenchora <helenchora43@gmail.com>
2023-11-22 00:10:51 +0600

    initial project files

---
+++ Controllers/NGOController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.EF;
+
+namespace ZeroHunger.Controllers
+{
+    public class NGOController : Controller
+    {
+        // GET: NGO
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Name, string Password)
+        {
+            using (var db = new Zero_HungerEntities())
+            {
+                var user = db.NGOs.SingleOrDefault(u => u.Name == Name && u.Password == Password);
+
+                if (user != null)
+                {
+                    Session["UserID"] = user.ID;
+                    Session["UserName"] = user.Name;
+                    return RedirectToAction("Index", "NGO");
+                }
+
+                ViewBag.Error = "Invalid username or password";
+                return View("Login");
+            }
+        }
+        public ActionResult logout()
+        {
+            Session.Clear();
+            return RedirectToAction("login");
+        }
+
+    }
+}
---
+++ DTO/NGOlogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZeroHunger.DTO
+{
+    public class NGOlogin
+    {
+        public class adminAccess : AuthorizeAttribute
+        {
+            protected override bool AuthorizeCore(HttpContextBase httpContext)
+            {
+                return httpContext.Session["user"] != null && httpContext.Session["type"].ToString().Equals("admin");
+            }
+        }
+
+    }
+}
